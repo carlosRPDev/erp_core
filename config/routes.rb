b6596446fc4710e8065cf38fe46d01df33b17372
Carlos Rodriguez <ucroxlive@gmail.com>
2025-11-13 12:54:49 -0500

    routes confirmation, reset account

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ ErpCore::Engine.routes.draw do
              module: :devise,
              path: "",
              path_names: { sign_in: "login", sign_out: "logout" },
-             controllers: { sessions: "erp_core/users/sessions" }
+             controllers: {
+              sessions: "erp_core/users/sessions",
+              registrations: "erp_core/users/registrations",
+              confirmations: "erp_core/users/confirmations"
+              }
 end
